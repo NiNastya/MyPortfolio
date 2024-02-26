469c7beccf30fb290abd10c767475702adedd953
Real Re <realre1337@gmail.com>
2024-02-26 11:36:44 +0600

    Fix Router

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
     {
-        path: "/",
+        path: "/MyPortfolio",
         name: "Home",
         component: () =>
             import("../views/Home.vue"),
